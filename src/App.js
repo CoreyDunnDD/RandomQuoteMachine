4138cd658e10d990487f45c0936ff002b96209f6
CoreyDunnDD <coreydunn.dd@gmail.com>
2022-11-13 20:57:02 -0500

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import quotes from './quotes'
+
+function newQuote() {
+  const quoteArrayLength = quotes.length
+  const whichQuote = Math.floor(Math.random() * quoteArrayLength)
+  const thisQuote = quotes[whichQuote]
+  const author = thisQuote.author
+  const quote = thisQuote.quote
+  document.getElementById("text").innerHTML = quote
+  document.getElementById("author").innerHTML = author
+  //Tweet Portion
+  const quoteInApiFormat = quote.replace(" ", "%20")
+  const authorInApiFormat = author.replace(" ", "%20")
+  let twitterLink = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
+  twitterLink += quoteInApiFormat
+  twitterLink += " -" + authorInApiFormat
+  document.getElementById("tweet-quote").href = twitterLink
+}
+
+window.onload = newQuote
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div id="quote-box" className="quote-box">
+        <h1>Random Quote</h1>
+          <p id="text"></p>
+          <p id="author"></p>
+        <button id="new-quote" onClick={newQuote}>New Quote</button>
+        <a href="https:\\www.google.com" target="_blank" rel="noreferrer" id="tweet-quote">
+          <button>Tweet It</button>
         </a>
-      </header>
-    </div>
+      </div>
   );
 }
 
